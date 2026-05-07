82f69c3a801225de444677a59d96494993ba5cd3
Ronaldo Bueno <ronaldobueno2002@gmail.com>
2026-05-07 10:29:36 -0300

    feat: adiciona configuracao de cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import { config } from './config';
 import { authMiddleware } from './middleware/auth';
@@ -8,6 +9,7 @@ import actionsRouter from './routes/index';
 import { swaggerSpec } from './swagger';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
